829a4d6b122dfaa82d66869f2faddee32fc6daf8
floe-s <fschettini@uade.edu.ar>
2023-01-22 20:40:13 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css'
 import Sidebar from './components/sidebar/Sidebar';
 import MainDash from './components/mainDash/MainDash';
 import RightSide from './components/rightSide/RightSide';
+import Footer from './components/footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
         <div className="AppGlass">
-        <Sidebar/>
-        <MainDash/>
-        <RightSide/>
-      </div>
+          <Sidebar/>
+          <MainDash/>
+          <RightSide/>
+        </div>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+const Footer = () => {
+  return(
+    <div className="Footer">
+      <span className='copy'>Copyright &copy; Dashboard 2023 Moidi Team6.</span>
+    </div>
+  )
+}
+
+export default Footer;
